ca869292fa1e644600b9d6d0fdb1ac07d4631441
mflinn-broad <mflinn@broadinstitute.org>
2022-12-01 11:09:38 -0500

    make it better

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -3,32 +3,30 @@ use crate::util;
 pub fn run() {
     let raw_input = util::read_input("inputs/day01.txt").unwrap();
     let input = process(raw_input);
-    println!("part 1: {}", part_1(input.clone()));
-    println!("part 2: {}", part_2(input));
+    println!("part 1: {}", part_1(&input));
+    println!("part 2: {}", part_2(&input));
 }
 
-fn process(input: String) -> Vec<Vec<u64>> {
+fn process(input: String) -> Vec<u64> {
     input.split("\n\n")
         .map(|elf| elf.lines()
-            .map(|calories| calories.parse().unwrap())
-            .collect()
+            .map(|calories| calories.parse::<u64>().unwrap())
+            .sum()
         )
         .collect()
 }
 
-fn part_1(input: Vec<Vec<u64>>) -> u64 {
+fn part_1(input: &[u64]) -> u64 {
     input.iter()
-        .map(|elf| elf.iter().sum())
-        .max().unwrap()
+        .max().unwrap().to_owned()
 }
 
-fn part_2 (input: Vec<Vec<u64>>) -> u64 {
-    let mut elves: Vec<u64> = input.iter()
-        .map(|elf| elf.iter().sum())
-        .collect();
+fn part_2 (input: &[u64]) -> u64 {
+    let mut sorted_elves = input.to_owned();
+    sorted_elves.sort();
     
-    elves.sort_by(|a, b| b.cmp(a));
-    elves.iter()
+    sorted_elves.iter()
+        .rev()
         .take(3)
         .sum()
 }
@@ -43,12 +41,12 @@ mod tests {
     #[bench]
     fn bench_part_1(b: &mut Bencher) {
         let input = process(util::read_input("inputs/day01.txt").unwrap());
-        b.iter(|| part_1(input.clone()));
+        b.iter(|| part_1(&input));
     }
 
     #[bench]
     fn bench_part_2(b: &mut Bencher) {
         let input = process(util::read_input("inputs/day01.txt").unwrap());
-        b.iter(|| part_2(input.clone()));
+        b.iter(|| part_2(&input));
     }
 }
