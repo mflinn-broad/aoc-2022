a5b09de9a01e846d98d7ed44e3c86b2cd4d9a3e3
mflinn-broad <mflinn@broadinstitute.org>
2022-12-04 09:41:19 -0500

    clippy and fmt

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,7 +1,5 @@
 use std::collections::HashSet;
 
-use itertools::Itertools;
-
 use crate::util;
 
 pub fn run() {
@@ -14,30 +12,36 @@ pub fn run() {
 type Pair = ((u32, u32), (u32, u32));
 
 fn process(input: String) -> Vec<Pair> {
-    input.lines()
+    input
+        .lines()
         .map(|pair_str| {
             let (first, second) = pair_str.split_once(',').unwrap();
             let (first_start, first_end) = first.split_once('-').unwrap();
             let (second_start, second_end) = second.split_once('-').unwrap();
-            ((first_start.parse().unwrap(), first_end.parse().unwrap()), (second_start.parse().unwrap(),second_end.parse().unwrap()))
+            (
+                (first_start.parse().unwrap(), first_end.parse().unwrap()),
+                (second_start.parse().unwrap(), second_end.parse().unwrap()),
+            )
         })
         .collect()
 }
 
 fn part_1(pairs: &[Pair]) -> usize {
-    pairs.iter()
-        .filter(|(first, second)| {    
-            (first.0 >= second.0 && first.1 <= second.1) 
-            || (second.0 >= first.0 && second.1 <= first.1)
+    pairs
+        .iter()
+        .filter(|(first, second)| {
+            (first.0 >= second.0 && first.1 <= second.1)
+                || (second.0 >= first.0 && second.1 <= first.1)
         })
         .count()
 }
 
 fn part_2(pairs: &[Pair]) -> usize {
-    pairs.iter()
+    pairs
+        .iter()
         .filter(|(first, second)| {
-            let first_range:HashSet<u32> = HashSet::from_iter(first.0..=first.1);
-            let second_range:HashSet<u32> = HashSet::from_iter(second.0..=second.1);
+            let first_range: HashSet<u32> = HashSet::from_iter(first.0..=first.1);
+            let second_range: HashSet<u32> = HashSet::from_iter(second.0..=second.1);
 
             first_range.intersection(&second_range).count() != 0
         })
