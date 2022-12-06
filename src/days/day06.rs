56c0b50c3fbfcc9230e2674865da3011e2c3a1f6
mflinn-broad <mflinn@broadinstitute.org>
2022-12-06 00:23:01 -0500

    day 6

---
+++ src/days/day06.rs
@@ -0,0 +1,23 @@
+use itertools::Itertools;
+
+use crate::util;
+use std::collections::HashSet;
+
+pub fn run() {
+    let raw_input = util::read_input("inputs/day06.txt").unwrap();
+    println!("part 1: {}", part_1(&raw_input, 4));
+    println!("part 2: {}", part_1(&raw_input, 14));
+}
+
+fn part_1(input: &str, window_size: usize) -> usize {
+    let input_chars = input.chars()
+        .collect_vec();
+
+    for (i, window) in input_chars.windows(window_size).enumerate(){
+        let window_char_set: HashSet<char> = HashSet::from_iter(window.iter().copied());
+        if window_char_set.len() == window_size {
+            return i + window_size;
+        }
+    }
+    0
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 pub fn run() {
     println!("Day 01 ***********");
@@ -15,4 +16,6 @@ pub fn run() {
     day04::run();
     println!("Day 05 ***********");
     day05::run();
+    println!("Day 06 ***********");
+    day06::run();
 }
